a302e07f558c79b04eda8cf8ce1501c450fa3864
Fahimul_Arefin <124819732+Fahimul-06@users.noreply.github.com>
2026-06-29 02:30:25 +0600

    Delete server directory

--- server/routes/categories.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import Category from '../models/Category.js';
-const router = express.Router();
-router.get('/', async (_req, res) => res.json({ categories: await Category.find().sort({ sortOrder: 1, name: 1 }) }));
-export default router;

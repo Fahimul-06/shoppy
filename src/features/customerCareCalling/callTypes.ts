b0b12c49da7290f623eb0542210f2434359ec25f
Fahimul_Arefin <124819732+Fahimul-06@users.noreply.github.com>
2026-06-30 23:55:16 +0600

    Delete src directory

--- src/features/customerCareCalling/callTypes.ts
+++
@@ -1,9 +0,0 @@
-export type Role = 'delivery' | 'admin';
-
-export type Signal = {
-  id?: string;
-  from: Role;
-  to?: Role;
-  type: 'offer' | 'answer' | 'candidate' | 'leave';
-  payload: any;
-};
